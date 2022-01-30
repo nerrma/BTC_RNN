cfdf0d8deab2e5c0d4f952c4c347affe19136c7c
Sina <sina.alsharif@gmail.com>
2022-01-30 12:35:37 +1100

    Update all files to latest

--- data.py
+++ data.py
@@ -10,11 +10,14 @@ import pandas_ta as ta
 
 
 SEQ_LEN = 60
-FUTURE_PREDICT_PERIOD = 2
+FUTURE_PREDICT_PERIOD = 1
 DATA_PATH = "Binance_BTCUSDT_1h.csv"
 
 pd.set_option('use_inf_as_na', True)
 
+def print_df(df):
+    for i in range(0, len(df)):
+        print(df.loc[i])
 
 def clean_dataset(df):
     assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
@@ -23,19 +26,20 @@ def clean_dataset(df):
     return df[indices_to_keep].astype(np.float64)
 
 def preprocess_df(df):
-    df.drop(['date', 'symbol', 'volume_usdt', 'future'], axis=1, inplace=True)
+    df.drop(['date', 'symbol', 'volume_usdt','tradecount','future'], axis=1, inplace=True)
 
     df = df.copy()
     for col in df.columns:
         if "RSI" in col:
-            print(df[col].values)
+            #print(df[col].values)
 
             df.loc[df[col] >= 70, col] = 1
             df.loc[df[col] <= 30, col] = -1 
             df.loc[(df[col] > 30) & (df[col] < 70), col] = 0  
             
 
-            print(df[col].values)
+            #print(df[col].values)
+            #print(df.head())
         elif col != "target":
 
             df[col] = df[col].pct_change()
@@ -45,6 +49,7 @@ def preprocess_df(df):
 
     df.dropna(inplace=True)
         
+    print(df.head())
     seq_data = []
     prev_days = deque(maxlen=SEQ_LEN)
 
@@ -82,7 +87,7 @@ def preprocess_df(df):
         X.append(seq)
         y.append(target)
 
-    print(df.head())
+    #print(df.head())
     return np.array(X), np.array(y)
 
 def classify(cur, fut):
@@ -105,31 +110,46 @@ except:
 if loaded == False:
     df = pd.read_csv(DATA_PATH, names=["unix", "date", "symbol", "open", "high", "low", "close", "volume_btc", "volume_usdt", "tradecount"], sep=",") 
     
-    df.ta.rsi(cumulative=True, append=True)
-    df.ta.macd(cumulative=True, append=True)
+    df = df.loc[::-1]
+    df.reset_index(drop=True, inplace=True)
     
-    print(df.columns)
-    print(df.head())
+    print(df.tail())
+
+    df.ta.rsi(cumulative=True, append=True)
+    df.ta.sma(cumulative=True, append=True)
+    df.ta.bbands(cumulative=True, append=True)
+    
+    df.ta.macd(cumulative=True, append=True)
     df.set_index('unix', inplace=True)
+    #df.ta.obv(cumulative=True, append=True)
+    
+    #print(df.columns)
+    #print(len(df.columns))
+    # df2 = df.iloc[[0, -1]]
+    #print(df2.head())
 
     df.fillna(method='ffill', inplace=True)
     df.dropna(inplace=True)
     
     
-    print(df.columns)
+    #print(df.columns)
     df['future'] = df['close'].shift(-FUTURE_PREDICT_PERIOD)
-    df['target'] = list(map(classify, df['close'], df['future']))
+    df['target'] = list(map(classify, df['open'], df['future']))
 
-    times = sorted(df.index.values)
+    times = sorted(df.index.values, reverse=False)
     last_5pct = times[-int(0.05*len(times))]
 
+    #print(f"Times, 5pct_len = {last_5pct}")
     val_df = df[(df.index >= last_5pct)]
     train_df = df[(df.index < last_5pct)]
+    print(f"val_df len = {len(val_df)}, train_df len = {len(train_df)}")
 
     train_X, train_y = preprocess_df(train_df)
-    val_X, val_y = preprocess_df(val_df)
+    val_X, val_y = preprocess_df(val_df) 
+    print(f"val_X len = {len(val_X)}")
 
     with open(f'{DATA_PATH}_saved_data.pkl', 'wb') as f:
         pickle.dump([train_X, train_y, val_X, val_y], f)
-
+    
+    val_df.to_pickle('val_df.pkl')
 
--- model.py
+++ model.py
@@ -2,48 +2,104 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from kerastuner.tuners import RandomSearch, BayesianOptimization
+#from kerastuner.hyperparameters import HyperParameters
+import pickle
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3.8 model.py <view/search>")
+    exit()
 
 assert tf.test.is_gpu_available() == True
 
 import data
 import time
-EPOCHS = 15
-BATCH_SIZE = 64
-NAME = f"HOURLY_BTC_{data.SEQ_LEN}-SEQ-{data.FUTURE_PREDICT_PERIOD}-BATCH-{BATCH_SIZE}-{int(time.time())}"
+EPOCHS = 20 
+BATCH_SIZE = 128 
+NAME = f"HOURLY_BTC_{data.SEQ_LEN}-SEQ-{data.FUTURE_PREDICT_PERIOD}-PERIOD-{BATCH_SIZE}-BATCH-{int(time.time())}"
 
-model = Sequential()
+LOG_DIR = f"KTUNER_logs/{NAME}"
+MAX_TRIALS = 10 
+EX_PER_TRIAL = 1
 
-model.add(LSTM(256, input_shape=(data.train_X.shape[1:]), return_sequences=True))
-model.add(BatchNormalization())
-model.add(Dropout(0.2))
+def build_model(hp):
+    model = Sequential()
 
-model.add(LSTM(256, input_shape=(data.train_X.shape[1:]), return_sequences=True))
-model.add(BatchNormalization())
-model.add(Dropout(0.3))
+    model.add(LSTM(hp.Int("input_units", 32, 256, 32),
+                   input_shape=(data.train_X.shape[1:]), 
+                   return_sequences=True))
+    model.add(Dropout(hp.Float("input_dropout", 0, 0.9, 1)))
+    model.add(BatchNormalization())
+   
+    num_layers = hp.Int("num_lstm", 1, 3, 1)
+    for i in range(0, num_layers):
+        lstm_units = hp.Int(f"lstm{i}_units", 32, 256, 32)
+        if i < num_layers-1:
+            model.add(LSTM(lstm_units,
+                           input_shape=(data.train_X.shape[1:]), 
+                           return_sequences=True))
+        else:
+            model.add(LSTM(lstm_units, 
+                           input_shape=(data.train_X.shape[1:]), 
+                           return_sequences=False))
 
-model.add(LSTM(128, input_shape=(data.train_X.shape[1:]), return_sequences=False))
-model.add(BatchNormalization())
-model.add(Dropout(0.2))
+        model.add(Dropout(hp.Float(f"lstm{i}_dropout", 0, 0.9, 0.1)))
+        model.add(BatchNormalization())
 
-model.add(Dense(32, activation="relu"))
-model.add(Dropout(0.2))
+    num_dense = hp.Int("num_dense", 0, 3, 1)
+    for i in range(0, num_dense):
+        model.add(Dense(hp.Int(f"dense{i}_units", 32, 128, 32), activation="relu"))
+        model.add(Dropout(hp.Float(f"dense{i}_dropout", 0, 0.9, 0.1)))
 
-model.add(Dense(2, activation="softmax"))
+    model.add(Dense(2, activation="softmax"))
 
-opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.001, decay=1e-6)
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-tb = TensorBoard(log_dir=f'logs/{NAME}', update_freq='epoch', profile_batch=0)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-filepath = "RNN_Final-test"
-checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc',
-                                                      verbose=1, save_best_only=True,
-                                                      mode='max')) # saves only the best one
+    return model
 
-history = model.fit(
-    data.train_X, data.train_y,
-    batch_size=BATCH_SIZE,
-    epochs=EPOCHS,
-    validation_data=(data.val_X, data.val_y),
-    callbacks=[tb, checkpoint]
-)
+
+if sys.argv[1] == "view":
+    tuner = pickle.load(open("ktuner_obj.pkl","rb"))
+
+    tuner.results_summary()
+ 
+elif sys.argv[1] == "search":
+
+    tb = TensorBoard(log_dir=f'logs/{NAME}', update_freq='epoch', profile_batch=0)
+
+    filepath = "RNN_Final-test"
+    checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc',
+                                                          verbose=1, save_best_only=True,
+                                                          mode='max')) # saves only the best one
+
+    tuner = BayesianOptimization(
+        build_model,
+        objective="val_accuracy",
+        max_trials=MAX_TRIALS,
+        executions_per_trial=EX_PER_TRIAL,
+        directory=LOG_DIR
+    )
+
+    tuner.search(x=data.train_X, y=data.train_y, 
+                 epochs=EPOCHS, 
+                 batch_size=BATCH_SIZE,
+                 validation_data=(data.val_X, data.val_y))
+
+    print(tuner.results_summary())
+
+    with open('ktuner_obj.pkl', 'wb') as f:
+        pickle.dump(tuner, f)
+else:
+    print("Usage: python3.8 model.py <view/search>")
+
+#model = build_model()
+# history = model.fit(
+#  data.train_X, data.train_y,
+#    batch_size=BATCH_SIZE,
+#    epochs=EPOCHS,
+#    validation_data=(data.val_X, data.val_y),
+#    callbacks=[tb, checkpoint]
+#)
